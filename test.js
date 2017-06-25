0c33444f63ba5871b1c0ef90e24c1d53e5a1317e
Sindre Sorhus <sindresorhus@gmail.com>
2017-06-25 23:33:24 +0200

    Init

---
+++ test.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import execa from 'execa';
+import m from '.';
+
+test('npm', async t => {
+	t.throws(() => {
+		m('.');
+	});
+
+	t.throws(() => {
+		m('cat-names');
+	});
+
+	t.regex(m('npm'), /npm/);
+
+	await execa('npm', ['install', '--global', 'cat-names']);
+	t.regex(m('cat-names'), /cat-names/);
+	await execa('npm', ['uninstall', '--global', 'cat-names']);
+});
+
+const testFailingCi = process.env.CI ? test.failing : test;
+testFailingCi('yarn', async t => {
+	await execa('npm', ['install', '--global', 'yarn']);
+
+	await execa('yarn', ['global', 'add', 'dog-names']);
+	t.regex(m('dog-names'), /dog-names/);
+	await execa('yarn', ['global', 'remove', 'dog-names']);
+});
+
+test('.silent()', t => {
+	t.is(m.silent('.'), null);
+});
